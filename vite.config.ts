84a964086562c8534d2469acfe3efa37300b9876
Merlijn Buit <merlijn.buit@infotopics.nl>
2024-12-02 16:04:21 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	plugins: [tsconfigPaths()],
-})
-
+  plugins: [tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html', // Main HTML file
+        config: './config.html', // Additional HTML file
+      },
+    },
+  },
+});
